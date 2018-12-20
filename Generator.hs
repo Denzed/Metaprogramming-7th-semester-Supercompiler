03b6b29af97759b2f9e3c4ef375fc1c197246700
Daniil Smirnov <dansmi97@gmail.com>
2018-12-20 20:00:33 +0300

    Updated generator

--- Generator.hs
+++ Generator.hs
@@ -9,40 +9,43 @@ residuate tree = (expr, program) where
 
 --- generation of residual program
 res :: NameSupply -> [(Conf, Conf)] -> Graph Conf -> (Conf, Program, NameSupply)
-res ns mp (Node e Stop) = (e, Program [] [], ns)
+res ns mp (Node e Stop) = (e, Program [], ns)
 
 res ns mp (Node (Ctr cname _) (Decompose ts)) = (Ctr cname args, p1, ns1) where
   (args, p1, ns1) = res' ns mp ts
 
-res ns mp (Node (Let (v, _) _) (Decompose ts)) = (e2 // [(v, e1)], p1, ns1) where
+res ns mp (Node (Let (v, _) _) (Decompose ts)) = (Let (v, e1) e2, p1, ns1) where
   ([e1, e2], p1, ns1) = res' ns mp ts
 
-res (n:ns) mp (Node e (Transient t)) = (fcall, Program ((FDef f1 vs body):fs) gs, ns1) where
+res ns mp (Node (e :@: e') (Decompose (var:es))) = (foldl (:@:) v es', p, ns') where 
+  (Node v@(Var _) _) = var
+  (es', p, ns') = res' ns mp es      
+
+res (n:ns) mp (Node e (Transient t)) = (call, Program ((FDef f1 vs body):fs), ns1) where
   vs = vnames e
   f1 = "ff" ++ (tail n)
-  fcall = FCall f1 $ map Var vs
-  (body, Program fs gs, ns1) = res ns ((e, fcall) : mp) t
+  call = Call f1 $ map Var vs
+  (body, Program fs, ns1) = res ns ((e, call) : mp) t
 
-res (n:ns) mp (Node e (Variants cs)) = (gcall, Program fs (newGs ++ gs), ns1) where
-  vs@(pv:vs') = vnames e
-  (vs_, vs'_) = if (isRepeated pv e) && (isUsed pv cs) then (pv:vs, vs) else (vs, vs')
-  g1 = "gg" ++ (tail n)
-  gcall = GCall g1 $ map Var vs_
-  (bodies, Program fs gs, ns1) = res' ns ((e, gcall) : mp) $ map snd cs
-  pats = [pat | (Contract v pat, _) <- cs]
-  newGs = [GDef g1 p vs'_ b | (p, b) <-  (zip pats bodies)]
-  isUsed vname cs = any (any (== vname) . vnames . nodeLabel . snd) cs
+res ns mp (Node (Lmb v e) (Transient t)) = (Lmb v e', p, ns') where
+  (e', p, ns') = res ns mp t
 
-res ns mp (Node e (Fold (Node base _) ren)) = (call, Program [] [], ns) where
+res ns mp (Node e (Variants cs)) = (e', p, ns') where
+  (Contract v _, _) = head cs
+  (ps, es)          = unzip $ map (\(Contract _ p, ep) -> (p, ep)) cs
+  (es', p, ns')     = res' ns mp es
+  e'                = Case (Var v) $ zip ps es' 
+
+res ns mp (Node e (Fold (Node base _) ren)) = (call, Program [], ns) where
   call = baseCall // [(x, Var y) | (x, y) <- ren]
   Just baseCall = lookup base mp
 
 -- proceeds a list of trees
 -- the main goal is to handle name supply
 res' :: NameSupply -> [(Conf, Conf)] -> [Graph Conf] -> ([Conf], Program, NameSupply)
-res' ns mp ts = foldl f ([], Program [] [], ns) ts where
-  f (cs, Program fs gs, ns1) t = (cs ++ [g], Program (fs ++ fs1) (gs ++ gs1), ns2) where
-    (g, Program fs1 gs1, ns2) = res ns1 mp t
+res' ns mp ts = foldl f ([], Program [], ns) ts where
+  f (cs, Program fs, ns1) t = (cs ++ [g], Program (fs ++ fs1), ns2) where
+    (g, Program fs1, ns2) = res ns1 mp t
 
 isBase e1 (Node _ (Decompose ts)) = or $ map (isBase e1) ts
 isBase e1 (Node _ (Variants cs)) = or $ map (isBase e1 . snd) cs
